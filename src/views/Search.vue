389fdb25547e48176a1a86c757a057e81b62d375
ellielok <ellielo2021@gmail.com>
2025-07-08 23:37:36 +1000

    Update frontend of Search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,14 +2,18 @@
   <div class="container py-4">
     <h2 class="mb-4">Find Support Near You</h2>
 
-    
+    <form>
+      <div class="input-group">
+        <input
+          v-model="query"
+          type="text"
+          class="form-control"
+          placeholder="Search for a suburb or postcode"
+          aria-label="Search"
+          required
+        />
+        <button class="btn btn-primary" type="submit">Search</button>
+      </div>
+    </form>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App'
-})
-</script>
